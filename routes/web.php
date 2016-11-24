c278377b68c4690675bd106979a192dfac97f97f
Jerome Sarmiento <jsarmiento@ltcglobal.com>
2016-11-24 15:22:00 +0800

    follow up for routes

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,12 @@
 | to using a Closure or controller method. Build something great!
 |
 */
-
-Route::get('/{cluster}', 'IndexController@index' );
-Route::post('/{cluster}', 'IndexController@store' );
 Route::get('reset-seed', function(){
     Artisan::call('migrate:refresh');
     Artisan::call('db:seed',['--class' => 'UsersTableSeeder']);
     dump('done!');
 } );
+Route::get('/{cluster}', 'IndexController@index' );
+Route::post('/{cluster}', 'IndexController@store' );
 Route::get('reset/{cluster}', 'IndexController@reset');
 
